e3a367b8fb49237349e3b361268542a57be28d8f
Ilyas Yamilev <14ilyas@mail.ru>
2019-07-06 18:32:36 +0500

    Added Contacts app

--- django_proj/contacts/views.py
+++ django_proj/contacts/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
 
 # Create your views here.
